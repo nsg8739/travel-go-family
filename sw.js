074f4badd81bead2c1f73849346f97906698974e
나 승권 <na@comtec.co.kr>
2026-07-20 14:25:05 +0900

    Use Grab logo for app shortcut

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='travel-go-v11';
-const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='travel-go-v12';
+const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icon.svg','./src/img/grab.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{if(response.ok&&new URL(event.request.url).origin===location.origin){const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy))}return response}).catch(()=>event.request.mode==='navigate'?caches.match('./index.html'):undefined)))});
