ef3dad562a62642e48883d74cc26a7c2ea99bd75
나 승권 <na@comtec.co.kr>
2026-07-20 15:29:39 +0900

    Rename travel information heading

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='travel-go-v22';
+const CACHE='travel-go-v23';
 const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icon.svg','./src/img/grab.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
